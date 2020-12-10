2dcff027dd59e212c901e21c54da07de89a9988b
Lenka <74135081+LenkaRo@users.noreply.github.com>
2020-12-10 09:11:03 +0100

    deleted nb.html file, added query into sql file

--- week_03/day_3/sql_programming_task_Lenka.sql
+++ week_03/day_3/sql_programming_task_Lenka.sql
@@ -50,6 +50,21 @@ WHERE email IS NULL OR email NOT LIKE '%@%'
 
 /* Which of the employees provided the most recent feedback? What was the date of this feedback? */
 
+SELECT 
+	e.first_name,
+	e.last_name,
+	f.date
+FROM
+	employees AS e INNER JOIN feedbacks AS f 
+	ON e.id = f.employee_id 
+ORDER BY date DESC NULLS LAST 
+LIMIT 1;
+
+/* You are chatting with one of the AcmeCorp employees, and she mentions that one of the teams 
+ * in the corporation has some employees whose stored first_name contains only their first initial, 
+ * e.g. “D” instead of “Dionne”, “C” instead of “Charles”. 
+ * Can you find the name of the team that she is most likely referring to 
+ * (i.e. which team has the highest number of employees with single initial first_names)`? */
 
 
 
